5414cbe6f0e0d9ba7dcaab864b7e02f59603bea5
Laura Nikulski <lauranikulski@gmail.com>
2025-03-17 23:23:01 +0900

    create table

--- localdb.py
+++ localdb.py
@@ -3,3 +3,16 @@ import sqlite3
 # create connection to db file
 myconnection = sqlite3.connect("mydb.db")  # Creates a file 'my_database.db'
 cursor = myconnection.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS films (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        release_year INTEGER NOT NULL,
+        rating INTEGER NOT NULL
+    )
+''')
+
+# Commit changes and close the connection
+myconnection.commit()
+myconnection.close()
