1c4826f28e610947061d2cca7b1ea60509ad19c6
Laura Nikulski <lauranikulski@gmail.com>
2025-03-23 22:12:56 +0900

    create table, add data

--- localdb.py
+++ localdb.py
@@ -2,17 +2,25 @@ import sqlite3
 
 # create connection to db file
 myconnection = sqlite3.connect("mydb.db")  # Creates a file 'my_database.db'
+
+# a cursor like so keeps track of the position in the result set
 cursor = myconnection.cursor()
 
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS films (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        release_year INTEGER NOT NULL,
-        rating INTEGER NOT NULL
-    )
-''')
+# Create a table
+cursor.execute('''CREATE TABLE IF NOT EXISTS films (
+                    id INTEGER PRIMARY KEY,
+                    name TEXT,
+                    release_year INTEGER
+                )''')
+
+# Insert some films
+cursor.execute("INSERT INTO films (id, name, release_year) VALUES (?, ?, ?)", 
+            (1, "The Matrix", 1999))
+cursor.execute("INSERT INTO films (id, name, release_year) VALUES (?, ?, ?)", 
+            (2, "Monster's, Inc.", 2001))
 
 # Commit changes and close the connection
 myconnection.commit()
 myconnection.close()
+
+
