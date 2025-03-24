5e8daea6c05f994f1ef658f72a444918f2c66562
Laura Nikulski <lauranikulski@gmail.com>
2025-03-24 23:13:23 +0900

    change to functions instead of simple script

--- localdb.py
+++ localdb.py
@@ -6,21 +6,37 @@ myconnection = sqlite3.connect("mydb.db")  # Creates a file 'my_database.db'
 # a cursor like so keeps track of the position in the result set
 cursor = myconnection.cursor()
 
-# Create a table
-cursor.execute('''CREATE TABLE IF NOT EXISTS films (
-                    id INTEGER PRIMARY KEY,
-                    name TEXT,
-                    release_year INTEGER
-                )''')
+# check for changes 
+print(myconnection.total_changes)
 
-# Insert some films
-cursor.execute("INSERT INTO films (id, name, release_year) VALUES (?, ?, ?)", 
-            (1, "The Matrix", 1999))
-cursor.execute("INSERT INTO films (id, name, release_year) VALUES (?, ?, ?)", 
-            (2, "Monster's, Inc.", 2001))
+if cursor.fetchone() is None: 
 
+# connect to database
+    with sqlite3.connect("mydb.db") as myconnection:
+        cursor = myconnection.cursor()
+    
+    # create a table
+        cursor.execute('''CREATE TABLE IF NOT EXISTS films (
+                        id INTEGER PRIMARY KEY,
+                        name TEXT,
+                        release_year INTEGER
+                    )''')
+
+    # add entries
+        cursor.execute("INSERT INTO films (id, name, release_year) VALUES (?, ?, ?)", 
+                (1, "The Matrix", 1999))
+        cursor.execute("INSERT INTO films (id, name, release_year) VALUES (?, ?, ?)", 
+                (2, "Monster's, Inc.", 2001))
+
+        with sqlite3.connect("mydb.db") as myconnection:
+            cursor = myconnection.cursor()
+            cursor.execute("SELECT * FROM films")
+            rows = cursor.fetchall()
+    
+            for row in rows:
+                print(row)  # Output: (1, 'The Matrix', 1999), (2, "Monster's, Inc.", 2001)
+    
+    
 # Commit changes and close the connection
 myconnection.commit()
 myconnection.close()
-
-
